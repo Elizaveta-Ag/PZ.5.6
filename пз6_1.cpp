9ab2e29f192db4695a1540f08a5f6a9024a95645
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 12:18:22 +0300

    Create пз6_1.cpp

---
+++ пз6_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int min = 60;
+    int hours = 2;
+    int res = min * hours;
+    cout << res << " minutes " << endl;
+
+    return 0;
+}
