c6c168c01291964a15c6b6395f226f81dd02947d
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 12:27:18 +0300

    Create пз6_4.cpp

---
+++ пз6_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int price;
+int num;
+int b
+cin >> price;
+cin >> num;
+int a = num * price;
+if (a > 1000){
+  b = a / 100 * 10;
+  cout << "The price is " << a - b;
+}
+else{
+  cout << "The price is " << a;
+}
+}
